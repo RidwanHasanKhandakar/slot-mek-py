87535908130a9b267df7640a31a073ae20df6d00
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-12 01:51:57 +0600

    made rows&cols global var & made symbol_count

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import random
 MAX_LINES=3
 MAX_BET=100
 MIN_BET=1
+ROWS=3
+COLS=3
+symbol_count={
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
 def deposit():
     while True:
         amount=input("What would you like to deposit[$]: ")
